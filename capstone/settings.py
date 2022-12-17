1febe8f420980284a9dbe37d408c3c9b4defe404
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2022-12-17 09:27:00 -0600

    Added models to track users and their authority, messages, and company info

--- capstone/settings.py
+++ capstone/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "phonenumber_field",
     'personnelPlanet',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -49,6 +50,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+AUTH_USER_MODEL = 'personnelPlanet.User'
+
 
 ROOT_URLCONF = 'capstone.urls'
 
--- personnelPlanet/models.py
+++ personnelPlanet/models.py
@@ -1,3 +1,48 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+
+
+class User(AbstractUser):
+    workId = models.CharField(max_length=6, default='000000')
+    phoneNumber = PhoneNumberField()
+    payRate = models.FloatField()
+    is_employer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Availability(models.Model):
+    employee = models.ForeignKey(User, on_delete=models.CASCADE)
+    day = models.CharField(max_length=10)
+    # If start and/or end is null we will count that as full/any time
+    start = models.TimeField(null=True, blank=True)
+    end = models.TimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+    employer = models.ForeignKey(User, on_delete=models.CASCADE)
+    employeeCount = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class EmployeeTracker(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    employee = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Messages(models.Model):
+    fromUser = models.IntegerField(blank=False, null=False)
+    toUser = models.IntegerField(blank=False, null=False)
+    content = models.CharField(max_length=350, blank=False, null=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
