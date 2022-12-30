7effcd92a0f8841d23622998f197b066b4836739
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2022-12-30 07:42:14 -0600

    A quick save before starting to implement channels for message room

--- capstone/settings.py
+++ capstone/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "phonenumber_field",
     'personnelPlanet',
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
