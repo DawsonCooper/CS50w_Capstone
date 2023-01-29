a053aebf16d2063e4d6a52cb7671a62d8ba4dec7
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-01-29 09:09:21 -0600

    Setting up for hosting

--- capstone/settings.py
+++ capstone/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8ua4)$^y9y+_h%gf&gvyo-15=)pblfqsb9!&0093a4gg$-1#)e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1:8000', 'personnel-planet.herokuapp.com']
 
